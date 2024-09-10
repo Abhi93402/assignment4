8c3208dd0a3fd9412c4dc1e37e1a4b80af2cb3dc
Abhi93402 <abhijeet93402@gmail.com>
2024-09-10 13:16:23 +0530

    Add files via upload

---
+++ assignment4/program3.dart
@@ -0,0 +1,10 @@
+void main() {
+  int sum = 0;
+  for (int x = 20; x <= 120; x++) {
+    sum = x + sum;
+    if (x % 2 != 0) {
+      print(sum);
+    }
+  }
+}
+
---
+++ assignment4/program7.dart
@@ -0,0 +1,8 @@
+void main() {
+  for (int x = 20; x <= 60; x++) {
+    if (x % 7 == 0) {
+      //x = x ^ 3;
+      print(x * x * x);
+    }
+  }
+}
